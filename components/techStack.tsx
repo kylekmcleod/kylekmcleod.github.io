e896c04bd758a95860d998e4c0198c43f0132a49
kylekmcleod <kylekmcleod1@gmail.com>
2024-06-30 16:55:13 -0700

    Add new tech stack icons to techStack.tsx

--- components/techStack.tsx
+++ components/techStack.tsx
@@ -3,6 +3,9 @@ import { Card } from "@nextui-org/card";
 import { Image } from "@nextui-org/image";
 import { Snippet } from "@nextui-org/snippet";
 
+import { FaWindows, FaJava, FaPython, FaJs, FaHtml5, FaCss3Alt, FaNodeJs, FaReact, FaDocker, FaGitAlt, FaGoogle } from "react-icons/fa";
+import { SiTypescript, SiMongodb, SiMysql, SiVisualstudiocode, SiEclipseide, SiFigma, SiPostman, SiNextdotjs, SiExpress } from "react-icons/si";
+
 export default function TechStack() {
   return (
     <div>
@@ -38,9 +41,34 @@ const TechStackButton = ({ label }: { label: string }) => (
     className="py-2 px-3 mt-2 transition-transform duration-200 ease-in-out transform hover:scale-105 hover:text-blue-500 hover:border-blue-500 cursor-default"
     style={{ marginRight: '4px', marginLeft: '4px' }}
   >
-    {label}
+    <span style={{ display: 'flex', alignItems: 'center' }}>
+      {icons[label]} <span style={{ marginLeft: '8px' }}>{label}</span>
+    </span>
   </Snippet>
 );
+
+const icons: { [key: string]: JSX.Element } = {
+  Windows: <FaWindows />,
+  Java: <FaJava />,
+  Python: <FaPython />,
+  JavaScript: <FaJs />,
+  TypeScript: <SiTypescript />,
+  HTML: <FaHtml5 />,
+  CSS: <FaCss3Alt />,
+  'Node.js': <FaNodeJs />,
+  React: <FaReact />,
+  'Next.js': <SiNextdotjs />,
+  MongoDB: <SiMongodb />,
+  MySQL: <SiMysql />,
+  'VS Code': <SiVisualstudiocode />,
+  Eclipse: <SiEclipseide />,
+  Git: <FaGitAlt />,
+  Docker: <FaDocker />,
+  'Google Cloud': <FaGoogle />,
+  Figma: <SiFigma />,
+  Postman: <SiPostman />,
+  'Express.js': <SiExpress />
+};
 
 
 
