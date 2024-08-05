b6a2b2f07d9c34c7596b9a4475a944a515ecddda
kylekmcleod <kylekmcleod1@gmail.com>
2024-08-04 19:42:41 -0700

    Footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
 
 export const metadata: Metadata = {
   title: {
@@ -47,15 +48,7 @@ export default function RootLayout({
             <main className="container mx-auto max-w-7xl px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-              </Link>
-            </footer>
+            <Footer />
           </div>
         </Providers>
       </body>
---
+++ components/footer.tsx
@@ -0,0 +1,41 @@
+import { Link } from "@nextui-org/link";
+import { GithubIcon, LinkedinIcon } from "@/components/icons";
+import { siteConfig } from "@/config/site";
+
+export const Footer = () => {
+  return (
+    <footer className="px-6 gap-4 w-full flex-row relative flex-nowrap justify-between h-[var(--navbar-height)] max-w-[1280px] mx-auto bg-base-200 py-6">
+      <div className="flex flex-col md:flex-row justify-between w-full">
+        <div className="flex flex-col items-left md:items-start">
+          <p className="font-bold text-lg mb-2">KYLE MCLEOD</p>
+          <p className="text-sm max-w-xs text-gray-500">kylekmcleod1@gmail.com</p>
+        </div>
+        <div className="flex gap-4 mt-4 md:mt-0">
+          <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+            <GithubIcon className="text-default-500" />
+          </Link>
+          <Link isExternal aria-label="LinkedIn" href={siteConfig.links.linkedin}>
+            <LinkedinIcon className="text-default-500" />
+          </Link>
+        </div>
+      </div>
+      <div className="mt-4 w-full">
+        <ul className="flex justify-left gap-4">
+          {siteConfig.navItems.map((item) => (
+            <li key={item.href}>
+              <Link
+                className="text-sm text-gray-600 hover:text-gray-800"
+                href={item.href}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="mt-4 text-sm text-gray-500 w-full text-center">
+        <p>&copy; {new Date().getFullYear()} Kyle McLeod. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
