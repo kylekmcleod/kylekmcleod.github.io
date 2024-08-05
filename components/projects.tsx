50d56dbcd861a348eb6f7e8f34e03a082bd91b2d
kylekmcleod <kylekmcleod1@gmail.com>
2024-08-04 20:22:26 -0700

    Animation projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,105 +1,93 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
+import { useInView } from "framer-motion";
 import { Card } from "@nextui-org/card";
 import { Image } from "@nextui-org/image";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   return (
     <div className="w-full mx-auto gap-10 grid grid-cols-1 md:grid-cols-2">
-      
-      {/* Project */}
-      <a href="https://github.com/kylekmcleod" target="_blank" rel="noreferrer" className="relative group">
-        <div className="flex flex-col justify-center h-full">
-          <h4 className="font-medium text-lg">Paper Trading App</h4>
-          <p className="text-sm uppercase font-bold mb-2 text-gray-500">Invest with zero risk</p>
-          <Card className="w-full">
-            <div className="relative w-full h-full">
-              <Image
-                removeWrapper
-                alt="Card background"
-                className="z-0 w-full h-full object-cover"
-                src="/images/paperTrader.png"
-              />
-              <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-            </div>
-          </Card>
-        </div>
-      </a>
-
-      {/* Project */}
-      <a href="https://github.com/kylekmcleod/Algorithmic-Trading-Learning" target="_blank" rel="noreferrer" className="relative group">
-        <div className="flex flex-col justify-center h-full">
-          <h4 className="font-medium text-lg">Algorithmic Stock Trading</h4>
-          <p className="text-sm uppercase font-bold mb-2 text-gray-500">Machine Learning Practice</p>
-          <Card className="w-full">
-            <div className="relative w-full h-full">
-              <Image
-                removeWrapper
-                alt="Card background"
-                className="z-0 w-full h-full object-cover"
-                src="/images/algTrading.png"
-              />
-              <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-            </div>
-          </Card>
-        </div>
-      </a>
-
-      {/* Project */}
-      <a href="https://github.com/kylekmcleod/Song-Seperator" target="_blank" rel="noreferrer" className="relative group">
-        <div className="flex flex-col justify-center h-full">
-          <h4 className="font-medium text-lg">Song Seperator</h4>
-          <p className="text-sm uppercase font-bold mb-2 text-gray-500">AI Music Splitter</p>
-          <Card className="w-full">
-            <div className="relative w-full h-full">
-              <Image
-                removeWrapper
-                alt="Card background"
-                className="z-0 w-full h-full object-cover"
-                src="/images/songSeperator.png"
-              />
-              <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-            </div>
-          </Card>
-        </div>
-      </a>
-      
-      {/* Project */}
-      <a href="https://github.com/kylekmcleod/Ticket-Apprentice" target="_blank" rel="noreferrer" className="relative group">
-        <div className="flex flex-col justify-center h-full">
-          <h4 className="font-medium text-lg">Ticket Apprentice</h4>
-          <p className="text-sm uppercase font-bold mb-2 text-gray-500">Event ticketing software</p>
-          <Card className="w-full">
-            <div className="relative w-full h-full">
-              <Image
-                removeWrapper
-                alt="Card background"
-                className="z-0 w-full h-full object-cover"
-                src="/images/ticketApprentice.png"
-              />
-              <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-            </div>
-          </Card>
-        </div>
-      </a>
-
-      {/* Project */}
-      <a href="https://github.com/kylekmcleod/E-Commerce-Watch-Store" target="_blank" rel="noreferrer" className="relative group">
-        <div className="flex flex-col justify-center h-full">
-          <h4 className="font-medium text-lg">Time Piece Vault</h4>
-          <p className="text-sm uppercase font-bold mb-2 text-gray-500">E-commerce watch store</p>
-          <Card className="w-full">
-            <div className="relative w-full h-full">
-              <Image
-                removeWrapper
-                alt="Card background"
-                className="z-0 w-full h-full object-cover"
-                src="/images/timePieceVault.png"
-              />
-              <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-            </div>
-          </Card>
-        </div>
-      </a>
+      {projects.map((project, index) => (
+        <AnimatedProjectCard
+          key={project.link}
+          project={project}
+          index={index}
+        />
+      ))}
     </div>
   );
 }
+
+const AnimatedProjectCard = ({ project, index }: { project: Project, index: number }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 30 }}
+      transition={{ delay: index * 0.2, duration: 0.5 }}
+      className="relative group"
+    >
+      <a href={project.link} target="_blank" rel="noreferrer" className="relative group">
+        <div className="flex flex-col justify-center h-full">
+          <h4 className="font-medium text-lg">{project.title}</h4>
+          <p className="text-sm uppercase font-bold mb-2 text-gray-500">{project.subtitle}</p>
+          <Card className="w-full">
+            <div className="relative w-full h-full">
+              <Image
+                removeWrapper
+                alt="Card background"
+                className="z-0 w-full h-full object-cover"
+                src={project.imageSrc}
+              />
+              <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+            </div>
+          </Card>
+        </div>
+      </a>
+    </motion.div>
+  );
+};
+
+const projects = [
+  {
+    title: "Paper Trading App",
+    subtitle: "Invest with zero risk",
+    link: "https://github.com/kylekmcleod",
+    imageSrc: "/images/paperTrader.png"
+  },
+  {
+    title: "Algorithmic Stock Trading",
+    subtitle: "Machine Learning Practice",
+    link: "https://github.com/kylekmcleod/Algorithmic-Trading-Learning",
+    imageSrc: "/images/algTrading.png"
+  },
+  {
+    title: "Song Seperator",
+    subtitle: "AI Music Splitter",
+    link: "https://github.com/kylekmcleod/Song-Seperator",
+    imageSrc: "/images/songSeperator.png"
+  },
+  {
+    title: "Ticket Apprentice",
+    subtitle: "Event ticketing software",
+    link: "https://github.com/kylekmcleod/Ticket-Apprentice",
+    imageSrc: "/images/ticketApprentice.png"
+  },
+  {
+    title: "Time Piece Vault",
+    subtitle: "E-commerce watch store",
+    link: "https://github.com/kylekmcleod/E-Commerce-Watch-Store",
+    imageSrc: "/images/timePieceVault.png"
+  }
+];
+
+type Project = {
+  title: string;
+  subtitle: string;
+  link: string;
+  imageSrc: string;
+};
